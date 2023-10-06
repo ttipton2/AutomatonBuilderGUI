e672b5e11af8c2dbd57528aba6c1c576bc15f8f8
Malcolm Anderson <malcolminyo@gmail.com>
2023-10-06 13:57:19 -0700

    More work on automaton window

--- src/components/ConfigureAutomatonWindow.tsx
+++ src/components/ConfigureAutomatonWindow.tsx
@@ -6,6 +6,19 @@ export default function ConfigureAutomatonWindow() {
             <div className="font-medium text-3xl">
                 Configure Automaton
             </div>
+            <div className="flow-root mb-5">
+                <label htmlFor="automaton-type" className="float-left">
+                    Automaton Type
+                    <div className="text-gray-300">
+                        A DFA will have additional requirements to be valid from
+                        an NFA.
+                    </div>
+                </label>
+                <select name="automaton-type" id="automaton-type" className="float-right">
+                    <option value="dfa">DFA</option>
+                    <option value="nfa">NFA</option>
+                </select>
+            </div>
             <DetailsBox_AlphabetEditor />
         </div>
     )
