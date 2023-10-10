953f922e849c22a913b09e330d23b2cb532b32cd
Malcolm Anderson <malcolminyo@gmail.com>
2023-10-09 21:51:26 -0700

    Toolbox made to be icons-only for now

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -2,45 +2,45 @@ import { Tool } from '../Tool';
 import ToolButton from './ToolButton';
 import StateManager from '../StateManager';
 
-import { BsArrowRight, BsCursorFill, BsDownload, BsPlusCircleFill, BsUpload } from 'react-icons/bs';
+import { BsArrowRight, BsCursor, BsCursorFill, BsDownload, BsNodePlus, BsNodePlusFill, BsPlusCircle, BsPlusCircleFill, BsUpload } from 'react-icons/bs';
 interface ToolboxProps {
     currentTool: Tool
     setCurrentTool: React.Dispatch<React.SetStateAction<Tool>>
 }
 
 export default function Toolbox(props: React.PropsWithChildren<ToolboxProps>) {
-    return (<div className='flex flex-col'>
+    return (<div className='flex flex-col text-xl'>
         <ToolButton tool={Tool.Select} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool}>
             <div className='flex flex-row items-center place-content-center'>
-                <BsCursorFill className='mr-1'/>
-                Select
+                {props.currentTool === Tool.Select ? <BsCursorFill /> : <BsCursor />}
+                {/* Select */}
             </div>
         </ToolButton>
         <ToolButton tool={Tool.States} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool}>
             <div className='flex flex-row items-center place-content-center'>
-                <BsPlusCircleFill className='mr-1'/>
-                Add States
+                {props.currentTool === Tool.States ? <BsPlusCircleFill /> : <BsPlusCircle />}
+                {/* Add States */}
             </div>
 
         </ToolButton>
         <ToolButton tool={Tool.Transitions} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool}>
             <div className='flex flex-row items-center place-content-center'>
-                <BsArrowRight className='mr-1'/>
-                Add Transitions
+                {props.currentTool === Tool.Transitions ? <BsNodePlusFill /> : <BsNodePlus />}
+                {/* Add Transitions */}
             </div>
         </ToolButton>
         <div className='grow'></div>
         <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center' onClick={StateManager.downloadJSON}>
             <div className='flex flex-row items-center place-content-center'>
-                <BsDownload className='mr-1'/>
-                Save
+                <BsDownload className='mr-0'/>
+                {/* Save */}
             </div>
         </button>
-        <input type='file' id='file-uploader' onChange={StateManager.uploadJSON}></input>
+        {/* <input type='file' id='file-uploader' onChange={StateManager.uploadJSON}></input> */}
         <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center'>
             <div className='flex flex-row items-center place-content-center'>
-                <BsUpload className='mr-1'/>
-                Load
+                <BsUpload className='mr-0'/>
+                {/* Load */}
             </div>
         </button>
     </div>);
