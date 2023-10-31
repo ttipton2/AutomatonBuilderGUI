5fd5ef45db9c5d8cede49a37a8b0edb6d5482a10
Malcolm Anderson <malcolminyo@gmail.com>
2023-10-31 16:32:53 -0700

    Started working on color scheme object

---
+++ src/LightColorScheme.tsx
@@ -0,0 +1,9 @@
+export let LightColorScheme = {
+    selectedNodeStrokeColor: '#018ED5',
+    selectedNodeStrokeWidth: 4,
+
+    nodeStrokeColor: 'black',
+    nodeStrokeWidth: 2,
+
+    nodeFill: 'white',
+};
