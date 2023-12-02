7ef786b13d23f64a4803f4495e341b19b63a9784
ryan-ball-360 <ryan.j.ball@wsu.edu>
2023-12-02 15:25:14 -0800

    Added Window for Testing Strings
    A modal window similar to the one for configuring an Automata has been added. In the window, the user can enter an input string, then press a button, which passes the string to a function that checks whether the string is accepted by the automaton or not (TestStringOnAutomata is currently just a skeleton function)

---
+++ src/components/TestStringOnAutomata.ts
@@ -0,0 +1,11 @@
+import DFAState from 'automaton-kit/lib/dfa/DFAState';
+import DFA from 'automaton-kit/lib/dfa/DFA';
+import DFATransition from 'automaton-kit/lib/dfa/DFATransition';
+import DFARunner, { DFARunnerStatus } from 'automaton-kit/lib/dfa/DFARunner';
+
+
+
+export function testStringOnAutomata(testString: string): void {
+    // Your testing logic here
+    console.log('Testing string:', testString);
+}
