64ce9e262c855947e8b91f93fe99c24ab3d4d3a1
ttipton2 <61717127+ttipton2@users.noreply.github.com>
2023-12-05 13:26:15 -0800

    test string logic finished
    Added logic to the test string function and updated the testWindow class so the it will display whether or not the string was accepted or rejected... Hard coded instances of the DFA, states, and transitions for testing purposes. Will need to integrate that into the GUI at a later date.

--- src/components/TestStringOnAutomata.ts
+++ src/components/TestStringOnAutomata.ts
@@ -5,7 +5,13 @@ import DFARunner, { DFARunnerStatus } from 'automaton-kit/lib/dfa/DFARunner';
 
 
 
-export function testStringOnAutomata(testString: string): void {
-    // Your testing logic here
-    console.log('Testing string:', testString);
+export function testStringOnAutomata(dfa: DFA, testString: string): string {
+    
+    // Logic is pretty straightforward. Creates an instance of a DFARunner, runs the DFA passed to it with the test
+    // string and returns the result to the test string window.
+    let runner = new DFARunner(dfa, testString.split(''));
+    runner.runUntilConclusion();
+    let result = runner.getStatus();
+
+    return result === DFARunnerStatus.Accepted ? 'Accepted' : 'Rejected';
 }
