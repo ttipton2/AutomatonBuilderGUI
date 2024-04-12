85b3943b958f471b311f8d2024250f62ec9eea70
ttipton2 <61717127+ttipton2@users.noreply.github.com>
2024-04-12 10:57:57 -0700

    added buttons for resetting the zoom to 100% and snapping stage back to its origin
    see title

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -2,8 +2,10 @@ import { Tool } from '../Tool';
 import ToolButton from './ToolButton';
 import StateManager from '../StateManager';
 import * as React from 'react';
-const {useRef} = React;
+const { useRef } = React;
 import { BsArrowRight, BsCursor, BsCursorFill, BsDownload, BsNodePlus, BsNodePlusFill, BsPlusCircle, BsPlusCircleFill, BsUpload } from 'react-icons/bs';
+import { TbZoomReset } from "react-icons/tb";
+import { BiReset } from "react-icons/bi";
 interface ToolboxProps {
     currentTool: Tool
     setCurrentTool: React.Dispatch<React.SetStateAction<Tool>>
@@ -17,40 +19,45 @@ export default function Toolbox(props: React.PropsWithChildren<ToolboxProps>) {
         fileInputRef.current?.click(); // Programmatically click the hidden file input
     };
 
-   
-   return (<div className='flex flex-col text-xl'>
-        <ToolButton tool={Tool.Select} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Select [S]">
-            <div className='flex flex-row items-center place-content-center'>
-                {props.currentTool === Tool.Select ? <BsCursorFill /> : <BsCursor />}
-                {/* Select */}
-            </div>
-        </ToolButton>
-        <ToolButton tool={Tool.States} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Add States [A]">
-            <div className='flex flex-row items-center place-content-center'>
-                {props.currentTool === Tool.States ? <BsPlusCircleFill /> : <BsPlusCircle />}
-                {/* Add States */}
-            </div>
+    // react-icons faSearchPlus and faSearchMinus for zoom in and out buttons
 
-        </ToolButton>
-        <ToolButton tool={Tool.Transitions} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Add Transitions [T]">
-            <div className='flex flex-row items-center place-content-center'>
-                {props.currentTool === Tool.Transitions ? <BsNodePlusFill /> : <BsNodePlus />}
-                {/* Add Transitions */}
-            </div>
-        </ToolButton>
-        <div className='grow'></div>
-        <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center' onClick={StateManager.downloadJSON} title="Download from JSON">
-            <div className='flex flex-row items-center place-content-center'>
-                <BsDownload className='mr-0'/>
-                {/* Save */}
-            </div>
-        </button>
-        <input type='file' id='file-uploader' ref={fileInputRef} style={{ display: 'none' }} onChange={StateManager.uploadJSON} />
-        <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center' onClick={handleLoadButtonClick} title="Load from JSON">
-            <div className='flex flex-row items-center place-content-center'>
-                <BsUpload className='mr-0'/>
-                {/* Load */}
-            </div>
-        </button>
-    </div>);
+    return (
+        <div className='flex flex-col text-xl'>
+            <ToolButton tool={Tool.Select} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Select [S]">
+                <div className='flex flex-row items-center place-content-center'>
+                    {props.currentTool === Tool.Select ? <BsCursorFill /> : <BsCursor />}
+                </div>
+            </ToolButton>
+            <ToolButton tool={Tool.States} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Add States [A]">
+                <div className='flex flex-row items-center place-content-center'>
+                    {props.currentTool === Tool.States ? <BsPlusCircleFill /> : <BsPlusCircle />}
+                </div>
+            </ToolButton>
+            <ToolButton tool={Tool.Transitions} setCurrentTool={props.setCurrentTool} currentTool={props.currentTool} title="Add Transitions [T]">
+                <div className='flex flex-row items-center place-content-center'>
+                    {props.currentTool === Tool.Transitions ? <BsNodePlusFill /> : <BsNodePlus />}
+                </div>
+            </ToolButton>
+            <div className='grow'></div>
+            <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center' onClick={StateManager.downloadJSON} title="Download from JSON">
+                <BsDownload />
+            </button>
+            <input type='file' id='file-uploader' ref={fileInputRef} style={{ display: 'none' }} onChange={StateManager.uploadJSON} />
+            <button className='rounded-full p-2 m-1 mx-2 block bg-amber-500 text-white text-center' onClick={handleLoadButtonClick} title="Load from JSON">
+                <BsUpload />
+            </button>
+            {/* Reset Zoom Button */}
+            <button className='rounded-full p-2 m-1 mx-2 block bg-blue-500 text-white text-center' onClick={StateManager.resetZoom} title="Reset Zoom">
+                <div className='flex flex-row items-center justify-center'>
+                    <TbZoomReset />
+                </div>
+            </button>
+            {/* Center Stage Button */}
+            <button className='rounded-full p-2 m-1 mx-2 block bg-green-500 text-white text-center' onClick={StateManager.centerStage} title="Center Stage">
+                <div className='flex flex-row items-center justify-center'>
+                    <BiReset />
+                </div>
+            </button>
+        </div>
+    );
 }
